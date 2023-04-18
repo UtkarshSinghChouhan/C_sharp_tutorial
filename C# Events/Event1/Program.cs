e1bd5eba0bb2280ed792d3a2ae192e41c6bf1ffb
Utkarsh Singh Chouhan <91064747+UtkarshSinghChouhan@users.noreply.github.com>
2023-04-18 11:23:51 +0530

    Events committed added

---
+++ C# Events/Event1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+namespace Event1;
+
+
+
+
+// Delegates enables a publiser and subscriber pattern where
+// delegate-object is the publisher and target methods are subscriber.
+
+
+// Events Formalize this publisher and subscriber pattern.
+
+// All the classes subscribed to the event are notified at once.
+
+
+
+
+
+
+
+
+
+
+
+// Declaring the Delegate
+public delegate void Transformer(int x);
+
+
+// ===================== PUBLISHER CLASS(which publishes the event) ===================
+
+
+class Notify{
+    // Declaring an event of Delegate Type
+    public event Transformer transformerEvent;
+
+    public void notifyOnCell(int x){
+        // if nobody has subscribed to the event it will return null
+        if(transformerEvent != null){
+           transformerEvent(x);
+        }
+    }
+
+};
+
+
+class subscriber1{
+    public static void Xhandler(int x){
+        Console.WriteLine("Event received by subscriber 1 object");
+    }
+};
+class subscriber2{
+    public static void Yhandler(int y){
+        Console.WriteLine("Event received by subscriber 2 object");
+    }
+};
+
+
+
+
+
+
+
+class Program{
+
+    public static void Square(int x){
+        Console.WriteLine($"The square is = {x * x}");
+    }
+
+
+
+    public static void Cuber(int x){
+        Console.WriteLine($"The cube is  = {x * x * x}");
+    }
+
+
+
+
+
+    public static void Main(string[] args){
+        Console.WriteLine("Enter a number :");
+        int inp = int.Parse(Console.ReadLine());
+
+
+
+        // Instatiating the Delegate and Binding functions with the Delegate
+        Transformer t = new Transformer(Square);
+        t += Cuber;
+        t(inp);
+
+
+
+
+        Notify n = new Notify();
+        // Hooking methods with the event
+        n.transformerEvent += subscriber1.Xhandler; //it means subscriber1 is subscribing to the event
+        n.transformerEvent += subscriber2.Yhandler; //it means subscriber2 is subscribing to the event
+
+        n.notifyOnCell(inp);
+    }
+}
---
+++ C# Events/Event2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+namespace Event2;
+
+
+// WHAT IS EVENT - Events are communicartion b/w two object via a notification mechanism.
+//                 Event provides notification ot the client when an object changes its state
+
+
+
+
+// Step - 01 : We need a PUBLISHER CLASS  (For puublishing the vent we need a Delegate and Based on that delegate we need an event)
+// step - 02 : We need  SUBSCRIBERS CLASSES
+
+
+
+
+
+// ======================== PUBLISHER CLASS ============================
+
+class EmployeeSeperator{
+    // Declaring a Delegate Based on the Separate() Method
+    public delegate void EmployeeSeperatedEventHandler();
+
+    // Declaring an Event based on the Delegate
+    public event EmployeeSeperatedEventHandler EmployeeSeperated;
+
+
+    // We can notify each subscriber class by calling this method
+    public void Separate(){
+
+        // Publishing the Event by= simply calling the event
+        if(EmployeeSeperated != null){
+            EmployeeSeperated(); 
+        }
+
+    }
+}
+
+
+
+
+
+
+// ============================= SUBSCRIBER CLASSES ==========================================
+
+class Finance{
+
+    // We need the instance of the publisher-class to call the event of that class
+    private EmployeeSeperator employeeSeperator;
+
+    public Finance(EmployeeSeperator employeeSeperator){
+        this.employeeSeperator = employeeSeperator;
+
+
+        // Hooking the method to the event [i.e. Subscribing to the event]
+        employeeSeperator.EmployeeSeperated += EmployeeSeperatedEventHandler;
+    }
+
+
+    // The Signature of the Handler methods must be similar to the Delegate
+    public void EmployeeSeperatedEventHandler(){
+        Console.WriteLine("Finance Department : employee Seperation process related to finance");
+    }
+}
+
+
+
+
+class IT{
+    private EmployeeSeperator employeeSeperator;
+    public IT(EmployeeSeperator employeeSeperator){
+        this.employeeSeperator = employeeSeperator;
+
+        // Hooking the method to the event [i.e. Subscribing to the event]
+        employeeSeperator.EmployeeSeperated += EmployeeSeperatedEventHandler;
+
+    }
+
+
+    // The Signature of the Handler methods must be similar to the Delegate
+    public void EmployeeSeperatedEventHandler(){
+        Console.WriteLine("IT Department : employee Seperation process related to IT");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+class Program{
+    public static void Main(string[] args){
+
+        
+        EmployeeSeperator employeeSeperator = new EmployeeSeperator();
+        Finance f = new Finance(employeeSeperator);
+        IT it = new IT(employeeSeperator);
+
+        employeeSeperator.Separate();
+
+    }
+}
